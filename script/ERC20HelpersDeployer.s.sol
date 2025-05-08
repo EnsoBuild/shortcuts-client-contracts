7398d8a0ce2dd6acb964aca5e9d7ea41ad500b7d
PeterMPhillips <github@pmphillips.ca>
2025-05-08 14:14:48 -0700

    deploy swap helpers

---
+++ script/ERC20HelpersDeployer.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import { ERC20Helpers } from "../src/helpers/ERC20Helpers.sol";
+import "forge-std/Script.sol";
+
+contract ERC20HelpersDeployer is Script {
+    function run() public returns (ERC20Helpers erc20Helpers) {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        erc20Helpers = new ERC20Helpers{ salt: "ERC20Helpers" }();
+
+        vm.stopBroadcast();
+    }
+}
