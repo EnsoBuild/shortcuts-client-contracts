f38fab2e97d1368f8c77d91d52c29c69f6248a04
PeterMPhillips <github@pmphillips.ca>
2025-05-19 10:21:51 -0700

    maverick update

--- src/helpers/MaverickV2Helpers.sol
+++ src/helpers/MaverickV2Helpers.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
+import { IERC20, SafeERC20 } from "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
+
 interface IMaverickV2Pool {
     struct AddLiquidityParams {
         uint8 kind;
@@ -55,34 +57,61 @@ interface IMaverickV2LiquidityManager {
  * @notice Helper contract to encode Maverick boosted positions
  */
 contract MaverickV2Helpers {
-    uint256 public constant VERSION = 1;
+    using SafeERC20 for IERC20;
 
-    function encodeAddLiquidityAndMintBoostedPosition(
+    uint256 public constant VERSION = 1;
+    uint256 BIP = 10000;
+
+    error InvalidBIP(uint256 amount);
+
+    function addLiquidityAndMintBoostedPosition(
         uint256 amountA,
+        uint256 amountB,
+        IERC20 tokenA,
+        IERC20 tokenB,
+        bool targetIsA,
+        uint256 targetBipModifier,
         address boostedPosition,
+        address manager,
         address lens,
-        address receiver
+        address receiver,
+        address refund
     )
-        public
-        view
-        returns (bytes memory)
+        external
     {
+        // get funds from msg.sender
+        tokenA.safeTransferFrom(msg.sender, address(this), amountA);
+        tokenB.safeTransferFrom(msg.sender, address(this), amountB);
+        // get target amount
+        uint256 targetAmount = targetIsA
+            ? amountA
+            : amountB;
+        // conditionally modify target amount
+        if (targetBipModifier > 0) {
+            if (targetBipModifier > BIP - 1) revert InvalidBIP(targetBipModifier);
+            targetAmount = (targetAmount * (BIP - targetBipModifier)) / BIP;
+        }
+        // get params
         IMaverickV2PoolLens.AddParamsSpecification memory addSpec = IMaverickV2PoolLens.AddParamsSpecification({
             slippageFactorD18: 0,
             numberOfPriceBreaksPerSide: 0,
-            targetAmount: amountA,
-            targetIsA: true
+            targetAmount: targetAmount,
+            targetIsA: targetIsA
         });
-
         (bytes memory packedSqrtPriceBreaks, bytes[] memory packedArgs,,,) =
             IMaverickV2PoolLens(lens).getAddLiquidityParamsForBoostedPosition(boostedPosition, addSpec);
-
-        return abi.encodeWithSelector(
-            IMaverickV2LiquidityManager.addLiquidityAndMintBoostedPosition.selector,
+        // approve manager to spend funds
+        tokenA.forceApprove(manager, amountA);
+        tokenB.forceApprove(manager, amountB);
+        // add liquidity (boostedPosition will be sent to receiver)
+        IMaverickV2LiquidityManager(manager).addLiquidityAndMintBoostedPosition(
             receiver,
             boostedPosition,
             packedSqrtPriceBreaks,
             packedArgs
         );
+        // refund remaining
+        tokenA.safeTransfer(refund, tokenA.balanceOf(address(this)));
+        tokenB.safeTransfer(refund, tokenB.balanceOf(address(this)));
     }
 }
