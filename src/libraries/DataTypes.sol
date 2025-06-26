c59292f12978d9ec9ea680b647cd59ec46f465e2
vnavascues <navascues.victor@gmail.com>
2025-06-26 08:55:36 +0200

    feat: added FeeSplitterDeployerEtherFi.s.sol

---
+++ src/libraries/DataTypes.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+library ChainId {
+    uint256 public constant ETHEREUM = 1;
+    uint256 public constant OPTIMISM = 10;
+    uint256 public constant BINANCE = 56;
+    uint256 public constant GNOSIS = 100;
+    uint256 public constant UNICHAIN = 130;
+    uint256 public constant POLYGON = 137;
+    uint256 public constant SONIC = 146;
+    uint256 public constant ZKSYNC = 324;
+    uint256 public constant WORLD = 480;
+    uint256 public constant HYPER = 999;
+    uint256 public constant SONEIUM = 1868;
+    uint256 public constant BASE = 8453;
+    uint256 public constant ARBITRUM = 42_161;
+    uint256 public constant AVALANCHE = 43_114;
+    uint256 public constant INK = 57_073;
+    uint256 public constant LINEA = 59_144;
+    uint256 public constant BERACHAIN = 80_094;
+    uint256 public constant PLUME = 98_866;
+    uint256 public constant SEPOLIA = 11_155_111;
+}
