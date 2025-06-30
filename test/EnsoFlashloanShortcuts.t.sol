b69ba651ba41e6294bdeba9a25df119113866c6d
Brenzee <karelisbrendons@gmail.com>
2025-06-30 16:19:11 +0300

    EnsoFlashloanShortcuts: add Euler vault flashloan support

--- test/EnsoFlashloanShortcuts.t.sol
+++ test/EnsoFlashloanShortcuts.t.sol
@@ -42,7 +42,8 @@ contract EnsoFlashloanShortcutsTest is Test, ERC721Holder, ERC1155Holder {
         _ethereumFork = vm.createFork(_rpcURL);
         vm.selectFork(_ethereumFork);
         shortcuts = new EnsoFlashloanShortcuts(
-            address(0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb) // Morpho
+            IMorpho(0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb),
+            IEulerGenericFactory(0x29a56a1b8214D9Cf7c5561811750D5cBDb45CC8e)
         );
     }
 
@@ -79,11 +80,12 @@ contract EnsoFlashloanShortcutsTest is Test, ERC721Holder, ERC1155Holder {
 
         state[0] = abi.encode(amount);
 
+        bytes memory morphoData = abi.encode(token, amount);
+
         shortcuts.flashLoan(
             FlashloanProtocols.Morpho,
-            address(token),
-            amount,
             user_bob,
+            morphoData,
             commands,
             state
         );
@@ -100,7 +102,7 @@ contract EnsoFlashloanShortcutsTest is Test, ERC721Holder, ERC1155Holder {
         IWETH token = IWETH(
             address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2)
         );
-        uint256 amount = 10 ** 18;
+        uint256 amount = 1 ether;
 
         bytes32[] memory commands = new bytes32[](2);
         bytes[] memory state = new bytes[](1);
@@ -121,25 +123,150 @@ contract EnsoFlashloanShortcutsTest is Test, ERC721Holder, ERC1155Holder {
 
         state[0] = abi.encode(amount);
 
+        bytes memory morphoData = abi.encode(token, amount);
+
         vm.startPrank(user_bob);
-        vm.deal(user_bob, 1 ether);
-        token.deposit{value: 1 ether}();
+        vm.deal(user_bob, amount);
+        token.deposit{value: amount}();
 
         // Simulate that router transfers tokens to flashloan contract
-        token.transfer(address(shortcuts), 1 ether);
+        token.transfer(address(shortcuts), amount);
 
         uint256 balanceBefore = token.balanceOf(receiver);
         shortcuts.flashLoan(
             FlashloanProtocols.Morpho,
-            address(token),
-            amount,
             receiver,
+            morphoData,
             commands,
             state
         );
         uint256 balanceAfter = token.balanceOf(receiver);
 
-        // Check that receiver received the excess WETH
-        assertEq(balanceAfter - balanceBefore, 1 ether);
+        assertEq(balanceAfter - balanceBefore, amount);
+    }
+
+    // Euler test:
+    // - flashlent asset: WETH
+    // - no other assets involved
+    // - WETH.withdraw -> WETH.deposit
+    function testEulerFlashLoan() public {
+        vm.selectFork(_ethereumFork);
+
+        // eWETH-2 Euler vault
+        address eulerVault = address(
+            0xD8b27CF359b7D15710a5BE299AF6e7Bf904984C2
+        );
+
+        IWETH token = IWETH(
+            address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2)
+        );
+        uint256 amount = 1 ether;
+
+        bytes32[] memory commands = new bytes32[](2);
+        bytes[] memory state = new bytes[](1);
+
+        // Unwrap -> Wrap
+        commands[0] = WeirollPlanner.buildCommand(
+            token.withdraw.selector,
+            0x01, // call
+            0x00ffffffffff, // 1 inputs
+            0xff, // no output
+            address(token)
+        );
+        commands[1] = WeirollPlanner.buildCommand(
+            token.deposit.selector,
+            0x03, // call
+            0x00ffffffffff, // 1 inputs
+            0xff, // no output
+            address(token)
+        );
+
+        state[0] = abi.encode(amount);
+
+        bytes memory eulerData = abi.encode(token, amount, eulerVault);
+
+        shortcuts.flashLoan(
+            FlashloanProtocols.Euler,
+            user_bob,
+            eulerData,
+            commands,
+            state
+        );
+    }
+
+    // Euler test:
+    // - flashlent asset: WETH
+    // - other assets: additional WETH
+    // - WETH.withdraw -> WETH.deposit
+    // - make sure excess is sent back to user
+    function testEulerFlashLoan_excess() public {
+        vm.selectFork(_ethereumFork);
+
+        // eWETH-2 Euler vault
+        address eulerVault = address(
+            0xD8b27CF359b7D15710a5BE299AF6e7Bf904984C2
+        );
+
+        IWETH token = IWETH(
+            address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2)
+        );
+        uint256 amount = 1 ether;
+
+        bytes32[] memory commands = new bytes32[](2);
+        bytes[] memory state = new bytes[](1);
+
+        // Unwrap -> Wrap
+        commands[0] = WeirollPlanner.buildCommand(
+            token.withdraw.selector,
+            0x01, // call
+            0x00ffffffffff, // 1 inputs
+            0xff, // no output
+            address(token)
+        );
+        commands[1] = WeirollPlanner.buildCommand(
+            token.deposit.selector,
+            0x03, // call
+            0x00ffffffffff, // 1 inputs
+            0xff, // no output
+            address(token)
+        );
+
+        state[0] = abi.encode(amount);
+
+        bytes memory eulerData = abi.encode(token, amount, eulerVault);
+
+        vm.startPrank(user_bob);
+        vm.deal(user_bob, amount);
+        token.deposit{value: amount}();
+
+        // Simulate that router transfers tokens to flashloan contract
+        token.transfer(address(shortcuts), amount);
+
+        uint256 balanceBefore = token.balanceOf(receiver);
+        shortcuts.flashLoan(
+            FlashloanProtocols.Euler,
+            receiver,
+            eulerData,
+            commands,
+            state
+        );
+
+        uint256 balanceAfter = token.balanceOf(receiver);
+
+        assertEq(balanceAfter - balanceBefore, amount);
+    }
+
+    // Euler test:
+    // - revert if attacker calls onFlashLoan with fake Euler vault
+    function testEulerFlashLoan_fakeEulerVault() public {
+        vm.selectFork(_ethereumFork);
+
+        address fakeEulerVault = address(0xdead);
+
+        bytes memory data = bytes("");
+
+        vm.prank(fakeEulerVault);
+        vm.expectRevert(EnsoFlashloanShortcuts.NotAuthorized.selector);
+        shortcuts.onFlashLoan(data);
     }
 }
