1c517c8734a24c1791b9f2157973f8744fd1df32
PeterMPhillips <github@pmphillips.ca>
2025-07-14 13:24:26 -0700

    use solady signature checker

--- src/paymaster/SignaturePaymaster.sol
+++ src/paymaster/SignaturePaymaster.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.20;
 
-import { SignatureVerifier } from "../libraries/SignatureVerifier.sol";
+import { SignatureCheckerLib } from "solady/utils/SignatureCheckerLib.sol";
+import { ECDSA } from "solady/utils/ECDSA.sol";
 import { UserOperationLib } from "account-abstraction/core/UserOperationLib.sol";
 import { IEntryPoint } from "account-abstraction/interfaces/IEntryPoint.sol";
 import { IPaymaster } from "account-abstraction/interfaces/IPaymaster.sol";
@@ -10,8 +11,6 @@ import { Ownable } from "openzeppelin-contracts/access/Ownable.sol";
 import { IERC20 } from "openzeppelin-contracts/token/ERC20/IERC20.sol";
 
 contract TestPaymaster is IPaymaster, Ownable {
-    using SignatureVerifier for bytes32;
-
     address private constant _NATIVE_ASSET = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 
     IEntryPoint public entryPoint;
@@ -57,9 +56,9 @@ contract TestPaymaster is IPaymaster, Ownable {
             uint256 amount,
             bytes calldata signature
         ) = parsePaymasterAndData(userOp.paymasterAndData);
-        bytes32 messageHash =
-            getHash(userOp, validUntil, validAfter, feeReceiver, token, amount).getEthSignedMessageHash();
-        address signer = messageHash.recoverSigner(signature);
+        bytes32 messageHash = getHash(userOp, validUntil, validAfter, feeReceiver, token, amount);
+        bytes32 ethSignedMessageHash = SignatureCheckerLib.toEthSignedMessageHash(messageHash);
+        address signer = ECDSA.recover(ethSignedMessageHash, signature);
         if (!validSigners[signer]) {
             return ("", _packValidationData(true, validUntil, validAfter));
         }
