ce75e2f02d04e34a09f0118bcc561d96d108204b
PeterMPhillips <github@pmphillips.ca>
2025-07-17 17:47:22 -0700

    change receiver to owner, reentrancy, base deploy

--- script/EnsoReceiverDeployer.s.sol
+++ script/EnsoReceiverDeployer.s.sol
@@ -3,18 +3,27 @@ pragma solidity ^0.8.28;
 
 import "../src/delegate/EnsoReceiver.sol";
 import "../src/factory/ERC4337CloneFactory.sol";
+import "../src/paymaster/SignaturePaymaster.sol";
 import "forge-std/Script.sol";
 
 contract EnsoReceiverDeployer is Script {
-    address ENTRY_POINT = address(0); // TODO
+    address ENTRY_POINT_V8 = 0x4337084D9E255Ff0702461CF8895CE9E3b5Ff108;
+    address ENTRY_POINT_V7 = 0x0000000071727De22E5E9d8BAf0edAc6f37da032;
 
-    function run() public returns (EnsoReceiver implementation, ERC4337CloneFactory factory) {
+    address OWNER = 0x826e0BB2276271eFdF2a500597f37b94f6c153bA; // TODO: set multisig as owner for production deployment
+    address BACKEND_SIGNER = 0xFE503EE14863F6aCEE10BCdc66aC5e2301b3A946; // TODO: in production the signer will have to set via the multisig
+
+    function run() public returns (EnsoReceiver implementation, ERC4337CloneFactory factory, SignaturePaymaster paymaster) {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
         vm.startBroadcast(deployerPrivateKey);
 
+        address entryPoint = ENTRY_POINT_V7;
         implementation = new EnsoReceiver{ salt: "EnsoReceiver" }();
-        factory = new ERC4337CloneFactory{ salt: "ERC4337CloneFactory" }(address(implementation), ENTRY_POINT);
+        implementation.initialize(address(0), address(0), address(0)); // brick the implementation
+        factory = new ERC4337CloneFactory{ salt: "ERC4337CloneFactory" }(address(implementation), entryPoint);
+        paymaster = new SignaturePaymaster{ salt: "SignaturePaymaster" }(IEntryPoint(entryPoint), OWNER);
+        paymaster.setSigner(BACKEND_SIGNER, true);
 
         vm.stopBroadcast();
     }
--- src/utils/Withdrawable.sol
+++ src/utils/Withdrawable.sol
@@ -10,27 +10,21 @@ import { ERC721Holder } from "openzeppelin-contracts/token/ERC721/utils/ERC721Ho
 abstract contract Withdrawable is ERC721Holder, ERC1155Holder {
     using SafeERC20 for IERC20;
 
-    address public receiver;
     IERC20 private constant _NATIVE_ASSET = IERC20(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE);
 
-    error OnlyReceiver(address sender);
-    error ReceiverNotSet();
+    error OnlyOwner(address sender);
     error WithdrawFailed();
 
-    modifier onlyReceiver() {
-        if (receiver == address(0)) revert ReceiverNotSet();
-        if (msg.sender != receiver) revert OnlyReceiver(msg.sender);
-        _;
-    }
-
     // @notice Withdraw native asset from this contract to the receiver
-    function withdrawNative() external onlyReceiver {
+    function withdrawNative() external {
+        _checkOwner();
         _withdrawNative(address(this).balance);
     }
 
     // @notice Withdraw ERC20s
     // @param erc20s An array of erc20 addresses
-    function withdrawERC20s(IERC20[] calldata erc20s) external onlyReceiver {
+    function withdrawERC20s(IERC20[] calldata erc20s) external {
+        _checkOwner();
         for (uint256 i; i < erc20s.length; ++i) {
             _withdrawERC20(erc20s[i], erc20s[i].balanceOf(address(this)));
         }
@@ -39,7 +33,8 @@ abstract contract Withdrawable is ERC721Holder, ERC1155Holder {
     // @notice Withdraw multiple ERC721 ids for a single ERC721 contract
     // @param erc721 The address of the ERC721 contract
     // @param ids An array of ids that are to be withdrawn
-    function withdrawERC721s(IERC721 erc721, uint256[] calldata ids) external onlyReceiver {
+    function withdrawERC721s(IERC721 erc721, uint256[] calldata ids) external {
+        _checkOwner();
         _withdrawERC721s(erc721, ids);
     }
 
@@ -47,16 +42,14 @@ abstract contract Withdrawable is ERC721Holder, ERC1155Holder {
     // @param erc1155 The address of the ERC155 contract
     // @param ids An array of ids that are to be withdrawn
     // @param amounts An array of amounts per id
-    function withdrawERC1155s(
-        IERC1155 erc1155,
-        uint256[] calldata ids,
-        uint256[] calldata amounts
-    )
-        external
-        onlyReceiver
-    {
+    function withdrawERC1155s(IERC1155 erc1155, uint256[] calldata ids, uint256[] calldata amounts) external {
+        _checkOwner();
         _withdrawERC1155s(erc1155, ids, amounts);
     }
+
+    function owner() public view virtual returns (address);
+
+    function _checkOwner() internal view virtual;
 
     function _withdrawToken(IERC20 token, uint256 amount) internal {
         if (token == _NATIVE_ASSET) {
@@ -67,22 +60,22 @@ abstract contract Withdrawable is ERC721Holder, ERC1155Holder {
     }
 
     function _withdrawNative(uint256 amount) internal {
-        (bool success,) = receiver.call{ value: amount }("");
+        (bool success,) = owner().call{ value: amount }("");
         if (!success) revert WithdrawFailed();
     }
 
     function _withdrawERC20(IERC20 erc20, uint256 amount) internal {
-        erc20.safeTransfer(receiver, amount);
+        erc20.safeTransfer(owner(), amount);
     }
 
     function _withdrawERC721s(IERC721 erc721, uint256[] memory ids) internal {
         for (uint256 i; i < ids.length; ++i) {
-            erc721.safeTransferFrom(address(this), receiver, ids[i]);
+            erc721.safeTransferFrom(address(this), owner(), ids[i]);
         }
     }
 
     function _withdrawERC1155s(IERC1155 erc1155, uint256[] memory ids, uint256[] memory amounts) internal {
         // safeBatchTransferFrom will validate the array lengths
-        erc1155.safeBatchTransferFrom(address(this), receiver, ids, amounts, "");
+        erc1155.safeBatchTransferFrom(address(this), owner(), ids, amounts, "");
     }
 }
