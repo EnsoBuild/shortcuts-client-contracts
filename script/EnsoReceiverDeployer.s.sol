4927f50e906411993a4d811c00ca016157b3409b
vnavascues <navascues.victor@gmail.com>
2025-07-18 15:44:47 +0200

    Feat/deterministic receiver safe tests (#38)
    * chore: simplify ShortcutDataTypes.sol
    
    * chore: justify ECDSA actions
    
    * chore: delete old test file
    
    * chore: improve tests organisation, and imports
    
    * feat: WIP - smart wallet test with Safe 1.5.0

--- script/EnsoReceiverDeployer.s.sol
+++ script/EnsoReceiverDeployer.s.sol
@@ -11,9 +11,13 @@ contract EnsoReceiverDeployer is Script {
     address ENTRY_POINT_V7 = 0x0000000071727De22E5E9d8BAf0edAc6f37da032;
 
     address OWNER = 0x826e0BB2276271eFdF2a500597f37b94f6c153bA; // TODO: set multisig as owner for production deployment
-    address BACKEND_SIGNER = 0xFE503EE14863F6aCEE10BCdc66aC5e2301b3A946; // TODO: in production the signer will have to set via the multisig
+    address BACKEND_SIGNER = 0xFE503EE14863F6aCEE10BCdc66aC5e2301b3A946; // TODO: in production the signer will have to
+        // set via the multisig
 
-    function run() public returns (EnsoReceiver implementation, ERC4337CloneFactory factory, SignaturePaymaster paymaster) {
+    function run()
+        public
+        returns (EnsoReceiver implementation, ERC4337CloneFactory factory, SignaturePaymaster paymaster)
+    {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
         vm.startBroadcast(deployerPrivateKey);
--- test/shortcuts/ShortcutDataTypes.sol
+++ test/shortcuts/ShortcutDataTypes.sol
@@ -1,29 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-library ShortcutDataTypes {
-    enum RoutingStrategy {
-        ENSOWALLET,
-        ROUTER,
-        DELEGATE,
-        EIP7702,
-        ROUTER_LEGACY,
-        DELEGATE_LEGACY
-    }
+enum RoutingStrategy {
+    ENSOWALLET,
+    ROUTER,
+    DELEGATE,
+    EIP7702,
+    ROUTER_LEGACY,
+    DELEGATE_LEGACY
+}
 
-    struct Shortcut {
-        RoutingStrategy routingStrategy;
-        address from;
-        address receiver;
-        address spender;
-        address[] tokensIn;
-        uint256[] amountsIn;
-        address[] tokensOut;
-        uint256 slippage;
-        uint256 fee;
-        address feeReceiver;
-        uint256 txGas;
-        bytes txData;
-        string referralCode;
-    }
+struct Shortcut {
+    RoutingStrategy routingStrategy;
+    address from;
+    address receiver;
+    address spender;
+    address[] tokensIn;
+    uint256[] amountsIn;
+    address[] tokensOut;
+    uint256 slippage;
+    uint256 fee;
+    address feeReceiver;
+    uint256 txGas;
+    bytes txData;
+    string referralCode;
 }
