945eec29835ee7b145b94928df261ccfbc36c314
vnavascues <navascues.victor@gmail.com>
2025-07-24 16:04:02 +0200

    chore: scripts to test enso_checkout

--- test/unit/concrete/factory/erc4337CloneFactory/getAddress.t.sol
+++ test/unit/concrete/factory/erc4337CloneFactory/getAddress.t.sol
@@ -4,7 +4,7 @@ import { ERC4337CloneFactory_Unit_Concrete_Test } from "./ERC4337CloneFactory.t.
 import { console2 } from "forge-std-1.9.7/Test.sol";
 
 contract ERC4337CloneFactory_GetAddress_Unit_Concrete_Test is ERC4337CloneFactory_Unit_Concrete_Test {
-    function test_ShouldReturnCounterfactualEnsoReceiverAddress() external {
+    function test_ShouldReturnCounterfactualEnsoReceiverAddress() external view {
         // it should return counterfactual EnsoReceiver address
         assertEq(s_cloneFactory.getAddress(s_owner), 0x3B371323Fc00bD13aF19D7269Ea86fF7bE2C3304);
     }
