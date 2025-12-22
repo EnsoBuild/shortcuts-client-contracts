7f944676520be490d5b50b39f1ce2a31727a1e44
vnavascues <navascues.victor@gmail.com>
2025-12-22 18:12:02 +0100

    chore" deploy on Monad

--- .bash/deploy.sh
+++ .bash/deploy.sh
@@ -24,7 +24,7 @@ fi
 if [[ $network_upper == "POLYGON" ]]; then
     params+=(--gas-estimate-multiplier 300)
 fi
-if [ "$broadcast" == "broadcast" ]; then
+if [ $broadcast == "broadcast" ]; then
     params+=(--broadcast)
     if [ -n "$verifier" ]; then
         params+=(--verify)
@@ -37,18 +37,21 @@ if [ "$broadcast" == "broadcast" ]; then
                 chain_id=9745
 =======
         params+=(--verifier "${verifier}")
-        if [ "$verifier" == "etherscan" ]; then
-            params+=(--etherscan-api-key "${!blockscan_key}")
-        elif [ "$verifier" == "routescan" ]; then
+        if [ $verifier == "etherscan" ]; then
+            params+=(--etherscan-api-key ${!blockscan_key})
+        elif [ $verifier == "routescan" ]; then
             params+=(--verifier-url "https://api.routescan.io/v2/network/mainnet/evm/80094/etherscan")
             params+=(--etherscan-api-key "verifyContract")
-        elif [ "$verifier" == "blockscout" ]; then
-            if [ "$network_upper" == "INK" ]; then
+        elif [ $verifier == "blockscout" ]; then
+            if [ $network_upper == "INK"]; then
                 params+=(--verifier-url "https://explorer.inkonchain.com/api")
-            elif [ "$network_upper" == "PLUME" ]; then
+                params+=(--etherscan-api-key ${!blockscan_key})
+            elif [ $network_upper == "PLUME"]; then
                 params+=(--verifier-url "https://explorer.plume.org/api")
-            elif [ "$network_upper" == "KATANA" ]; then
+                params+=(--etherscan-api-key ${!blockscan_key})
+            elif [ $network_upper == "KATANA"]; then
                 params+=(--verifier-url "https://explorer.katanarpc.com/api")
+<<<<<<< HEAD
 >>>>>>> 3975f88 (feat: added EnsoCCIPReceiver tests)
             else
                 printf '%s\n' "Invalid routescan network" >&2
@@ -70,6 +73,16 @@ if [ "$broadcast" == "broadcast" ]; then
                 else
                     params+=(--verifier-url "https://${network}.blockscout.com/api")
                 fi
+=======
+                params+=(--etherscan-api-key ${!blockscan_key})
+            else
+                params+=(--verifier-url "https://${network}.blockscout.com/api")
+                params+=(--etherscan-api-key ${!blockscan_key})
+            fi
+        elif [ $verifier == "sourcify"]; then
+            if [ $network_upper == "MONAD"]; then
+                params+=(--verifier-url "https://sourcify-api-monad.blockvision.org/")
+>>>>>>> b00c98f (chore" deploy on Monad)
             fi
         fi
     fi
