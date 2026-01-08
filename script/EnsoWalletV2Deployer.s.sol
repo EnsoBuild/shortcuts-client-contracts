e073e92055c4f93f7e4e1b25f6a647adccc59064
vnavascues <navascues.victor@gmail.com>
2026-01-08 10:18:54 +0100

    chore: run forge fmt

--- script/EnsoWalletV2Deployer.s.sol
+++ script/EnsoWalletV2Deployer.s.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-import "../src/wallet/EnsoWalletV2.sol";
 import "../src/factory/EnsoWalletV2Factory.sol";
+import "../src/wallet/EnsoWalletV2.sol";
 import "forge-std/Script.sol";
 
 contract EnsoWalletV2Deployer is Script {
