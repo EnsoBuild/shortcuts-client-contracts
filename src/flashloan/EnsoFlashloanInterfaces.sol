d7cdf7a0bc8a4add99cf7f6324c141cffb14f6e6
Brenzee <karelisbrendons@gmail.com>
2026-01-11 13:43:21 +0200

    AbstractEnsoFlashloan and EnsoWalletFlashloanAdapter contracts, tests for EnsoWalletFlashloanAdapter

--- src/flashloan/EnsoFlashloanInterfaces.sol
+++ src/flashloan/EnsoFlashloanInterfaces.sol
@@ -1,34 +1,20 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.28;
 
-interface IERC3156FlashBorrower {
-    function onFlashLoan(
-        address initiator,
-        address token,
-        uint256 amount,
-        uint256 fee,
-        bytes calldata data
-    )
-        external
-        returns (bytes32);
-}
+// interface IERC3156FlashBorrower {
+//     function onFlashLoan(
+//         address initiator,
+//         address token,
+//         uint256 amount,
+//         uint256 fee,
+//         bytes calldata data
+//     )
+//         external
+//         returns (bytes32);
+// }
 
 interface IMorpho {
     function flashLoan(address token, uint256 assets, bytes calldata data) external;
-}
-
-interface IEVault {
-    function flashLoan(uint256 amount, bytes calldata data) external;
-}
-
-interface IBalancerV2Vault {
-    function flashLoan(
-        address recipient,
-        address[] memory tokens,
-        uint256[] memory amounts,
-        bytes memory userData
-    )
-        external;
 }
 
 interface IAaveV3Pool {
@@ -43,3 +29,20 @@ interface IAaveV3Pool {
     )
         external;
 }
+
+struct BalancerV3FlashloanParams {
+    address wallet;
+    address[] tokens;
+    uint256[] amounts;
+    bytes32 accountId;
+    bytes32 requestId;
+    bytes32[] commands;
+    bytes[] state;
+}
+
+interface IBalancerV3Vault {
+    function unlock(bytes calldata data) external;
+    function sendTo(address token, address to, uint256) external;
+    function settle(address token, uint256 amount) external;
+}
+
