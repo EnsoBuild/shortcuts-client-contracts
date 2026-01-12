f3b87763ef33ee8644d1a95d34a9d2cf48de6c54
Brenzee <karelisbrendons@gmail.com>
2026-01-12 15:37:45 +0200

    EnsoWalletFlashloanAdapter deployment script for Berachain and Ethereum

---
+++ script/EnsoWalletFlashloanAdapterDeployer.s.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import "../src/flashloan/EnsoWalletFlashloanAdapter.sol";
+import "forge-std/Script.sol";
+
+contract EnsoWalletFlashloanAdapterDeployer is Script {
+    uint256 constant ETHEREUM = 1;
+    uint256 constant BERACHAIN = 80_094;
+
+    mapping(uint256 chainId => address[] lenders) internal _lenders;
+    mapping(uint256 chainId => LenderProtocol[] protocols) internal _protocols;
+
+    function _initConfigs() internal {
+        // Ethereum
+        _lenders[ETHEREUM].push(0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2); // Aave V3
+        _protocols[ETHEREUM].push(LenderProtocol.AaveV3);
+
+        _lenders[ETHEREUM].push(0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb); // Morpho
+        _protocols[ETHEREUM].push(LenderProtocol.Morpho);
+
+        _lenders[ETHEREUM].push(0xbA1333333333a1BA1108E8412f11850A5C319bA9); // Balancer V3
+        _protocols[ETHEREUM].push(LenderProtocol.BalancerV3);
+
+        _lenders[ETHEREUM].push(0x6Bd780E7fDf01D77e4d475c821f1e7AE05409072); // Dolomite
+        _protocols[ETHEREUM].push(LenderProtocol.Dolomite);
+
+        // Berachain
+        _lenders[BERACHAIN].push(0x24147243f9c08d835C218Cda1e135f8dFD0517D0); // Bend (Morpho fork)
+        _protocols[BERACHAIN].push(LenderProtocol.Morpho);
+
+        _lenders[BERACHAIN].push(0x003Ca23Fd5F0ca87D01F6eC6CD14A8AE60c2b97D); // Dolomite
+        _protocols[BERACHAIN].push(LenderProtocol.Dolomite);
+    }
+
+    function run() public returns (EnsoWalletFlashloanAdapter adapter) {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        _initConfigs();
+
+        address[] storage lenders = _lenders[block.chainid];
+        LenderProtocol[] storage protocols = _protocols[block.chainid];
+
+        require(lenders.length > 0, "Unsupported chain");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        adapter = new EnsoWalletFlashloanAdapter{ salt: "EnsoWalletFlashloanAdapter" }(lenders, protocols);
+
+        vm.stopBroadcast();
+    }
+}
