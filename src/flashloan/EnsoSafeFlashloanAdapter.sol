d336f7070b7b31806c9f0ceabbf679d6e16202d9
Brenzee <karelisbrendons@gmail.com>
2026-01-12 16:15:45 +0200

    EnsoSafeFlashloanAdapter: flashloan adapter for Safe wallets using module pattern

---
+++ src/flashloan/EnsoSafeFlashloanAdapter.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.28;
+
+import { AbstractEnsoShortcuts } from "../AbstractEnsoShortcuts.sol";
+import { AbstractEnsoFlashloan, LenderProtocol } from "./AbstractEnsoFlashloan.sol";
+import { ISafe } from "safe-smart-account-1.5.0/interfaces/ISafe.sol";
+import { Enum } from "safe-smart-account-1.5.0/libraries/Enum.sol";
+
+contract EnsoSafeFlashloanAdapter is AbstractEnsoFlashloan {
+    error SafeExecutionFailed();
+
+    address public immutable shortcuts;
+
+    constructor(
+        address[] memory lenders,
+        LenderProtocol[] memory protocols,
+        address shortcuts_
+    )
+        AbstractEnsoFlashloan(lenders, protocols)
+    {
+        shortcuts = shortcuts_;
+    }
+
+    function executeShortcut(
+        address wallet,
+        bytes32 accountId,
+        bytes32 requestId,
+        bytes32[] memory commands,
+        bytes[] memory state
+    )
+        internal
+        override
+    {
+        bytes memory data = abi.encodeCall(
+            AbstractEnsoShortcuts.executeShortcut,
+            (accountId, requestId, commands, state)
+        );
+
+        bool success = ISafe(payable(wallet)).execTransactionFromModule(
+            shortcuts,
+            0,
+            data,
+            Enum.Operation.DelegateCall
+        );
+
+        if (!success) revert SafeExecutionFailed();
+    }
+}
