57a7982966466a8ded4d3d1909c71b635eb0b388
Brenzee <karelisbrendons@gmail.com>
2026-01-13 12:15:23 +0200

    fix test imports

--- test/EnsoWalletFlashloanAdapter.t.sol
+++ test/EnsoWalletFlashloanAdapter.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-import "../lib/forge-std/src/Test.sol";
+import { Test } from "forge-std-1.9.7/Test.sol"; // THIS
 
 import "../src/factory/EnsoWalletV2Factory.sol";
 import "../src/flashloan/AbstractEnsoFlashloan.sol";
